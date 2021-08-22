f1f81d6214b00dd52fd03c8c90781cb843b08475
Bruno Felix <brunofelix.ml@gmail.com>
2021-08-22 11:08:42 -0300

    feat: adding user delete action

--- src/views/UseList.js
+++ src/views/UseList.js
@@ -1,22 +1,51 @@
-import React from 'react';
-import { Text, View, FlatList } from 'react-native';
-import { ListItem, Avatar } from 'react-native-elements';
+import React, { useState } from 'react';
+import { View, FlatList } from 'react-native';
+import { ListItem, Avatar, Button } from 'react-native-elements';
 
-import users from '../data/users';
+import usersData from '../data/users';
 
 function UserList(props) {
+	const [users, setUsers] = useState(usersData);
+
+	const rightContent = user => (
+		<Button
+			title="Deletar"
+			icon={{ name: 'delete', color: 'white' }}
+			buttonStyle={style.buttonStyleDelete}
+			onPress={() => deleteUser(user.id)}
+		/>
+	);
+
+	const leftContent = user => (
+		<Button
+			title="Editar"
+			icon={{ name: 'edit', color: 'white' }}
+			buttonStyle={style.buttonStyleDelete}
+			onPress={() => console.warn(user.id)}
+		/>
+	);
+
+	function deleteUser(userId) {
+		const remaningUsers = users
+			.filter(({ id }) => id !== userId)
+			.map(user => user);
+		setUsers(remaningUsers);
+	}
+
 	function getUserItem({ item: user }) {
 		return (
-			<ListItem
+			<ListItem.Swipeable
 				bottomDivider
 				onPress={() => props.navigation.navigate('UsersForm')}
+				rightContent={rightContent(user)}
+				leftContent={leftContent(user)}
 			>
 				<Avatar source={{ uri: user.avatarUrl }} />
 				<ListItem.Content>
 					<ListItem.Title>{user.name}</ListItem.Title>
 					<ListItem.Subtitle>{user.email}</ListItem.Subtitle>
 				</ListItem.Content>
-			</ListItem>
+			</ListItem.Swipeable>
 		);
 	}
 
@@ -32,3 +61,8 @@ function UserList(props) {
 }
 
 export default UserList;
+
+const style = {
+	buttonStyleDelete: { minHeight: '100%', backgroundColor: 'red' },
+	buttonStyleEdit: { minHeight: '100%', backgroundColor: 'green' },
+};
